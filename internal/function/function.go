0071d987a22cde1543ea9ea740b543ede561660f
JasVv <>
2020-03-16 21:34:20 +0900

    関数②

--- internal/function/function.go
+++ internal/function/function.go
@@ -1,10 +1,25 @@
 package function
 
 import (
+	"errors"
 	"fmt"
 )
 
-// Hello : print Hello to console
+// Hello : コンソールにhelloを出力
 func Hello() {
 	fmt.Println("hello")
+}
+
+// Add : 整数2つを受け取り、足した結果を返す
+func Add(x, y int) int {
+	return x + y
+}
+
+// Div : 整数x, yを受け取り、xをyで割った結果を返す yが0の時はエラー
+func Div(x, y int) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("divide by zero")
+	}
+
+	return float64(x) / float64(y), nil
 }
--- internal/function/function_test.go
+++ internal/function/function_test.go
@@ -4,6 +4,26 @@ import (
 	"testing"
 )
 
+// ファイル名はxxx_test.goにする
+// 関数名はTestから始める
 func TestHello(t *testing.T) {
 	Hello()
+}
+
+func TestAdd(t *testing.T) {
+	if i := Add(10, 20); i == 30 {
+		t.Log("correct")
+	} else {
+		t.Errorf("expected 30, actual %#v", i)
+	}
+}
+
+func TestDiv(t *testing.T) {
+	if f, err := Div(6, 2); err != nil {
+		t.Fatalf("fatal error %#v", err)
+	} else if f != 3 {
+		t.Errorf("expected 3, actual %#v", f)
+	} else {
+		t.Log("correct")
+	}
 }
